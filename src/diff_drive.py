6d0177cf85a32282850f3fa274e2e2e7c9ac36c1
MoffKalast <vid.rijavec@gmail.com>
2025-08-30 17:13:31 +0200

    converted to effort

--- src/diff_drive.py
+++ src/diff_drive.py
@@ -25,9 +25,10 @@ class DiffDrive:
 
 		self.SPEED_MIN = rospy.get_param('~min_speed', 0.0)
 		self.SPEED_MAX = rospy.get_param('~max_speed', 1.0)
+		self.SPEED_SCALE = rospy.get_param('~speed_scale', 2.0)
 
 		self.cmdsub = rospy.Subscriber("cmd_vel", Twist, self.velocity)
-		self.wheel_pub = rospy.Publisher("diff_drive", JointState, queue_size=1)
+		self.wheel_pub = rospy.Publisher("motor_cmd", JointState, queue_size=1)
 
 		self.reconfigure_server = DynamicReconfigureServer(DiffDriveSimpleConfig, self.dynamic_reconfigure_callback)
 
@@ -59,8 +60,7 @@ class DiffDrive:
 
 		if msg.angular.z != 0 or msg.linear.x != 0:
 
-			#consistency with diff_drive_controller, which does not reverse correctly
-			#fix the angular inversion in the twist publisher, since it's the only way to make it compatible with both
+			#consistency with diff_drive_controller, which does not reverse the same way
 			if msg.linear.x < 0:
 				msg.angular.z = -msg.angular.z
 
@@ -76,53 +76,56 @@ class DiffDrive:
 			left_vel = math.copysign(left_spd, left)
 			right_vel = math.copysign(right_spd, right)
 
+		left_effort = clamp(left_vel / self.SPEED_SCALE, -1.0, 1.0)
+		right_effort = clamp(right_vel / self.SPEED_SCALE, -1.0, 1.0)
+
 		state = JointState()
-		state.name = ["left_wheel", "right_wheel"]
-		state.velocity = [left_vel, right_vel]
+		state.name = ["left_thrust", "right_thrust"]
+		state.effort = [left_effort, right_effort]
 		self.wheel_pub.publish(state)
 
 
 	def diffdrive(self, x,  y):
 
-			x = clamp(x, -self.SPEED_MAX, self.SPEED_MAX)
-			y = clamp(y, -self.SPEED_MAX, self.SPEED_MAX)
+		x = clamp(x, -self.SPEED_MAX, self.SPEED_MAX)
+		y = clamp(y, -self.SPEED_MAX, self.SPEED_MAX)
 
-			# First Compute the angle in deg
-			# First hypotenuse
-			z = math.sqrt(x * x + y * y)
+		# First Compute the angle in deg
+		# First hypotenuse
+		z = math.sqrt(x * x + y * y)
 
-			# angle in radians
-			if z == 0:
-				rad = 0
-			else:
-				rad = math.acos(math.fabs(x) / z)
+		# angle in radians
+		if z == 0:
+			rad = 0
+		else:
+			rad = math.acos(math.fabs(x) / z)
 
-			# and in degrees
-			angle = rad * 180 / math.pi
+		# and in degrees
+		angle = rad * 180 / math.pi
 
-			# Now angle indicates the measure of turn
-			# Along a straight line, with an angle o, the turn co-efficient is same
-			# this applies for angles between 0-90, with angle 0 the coeff is -1
-			# with angle 45, the co-efficient is 0 and with angle 90, it is 1
-			tcoeff = -1 + (angle / 90) * 2
-			turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
-			turn = round(turn * 100, 0) / 100
+		# Now angle indicates the measure of turn
+		# Along a straight line, with an angle o, the turn co-efficient is same
+		# this applies for angles between 0-90, with angle 0 the coeff is -1
+		# with angle 45, the co-efficient is 0 and with angle 90, it is 1
+		tcoeff = -1 + (angle / 90) * 2
+		turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
+		turn = round(turn * 100, 0) / 100
 
-			# And max of y or x is the movement
-			mov = max(math.fabs(y), math.fabs(x))
+		# And max of y or x is the movement
+		mov = max(math.fabs(y), math.fabs(x))
 
-			# First and third quadrant
-			if (x >= 0 and y >= 0) or (x < 0 and y < 0):
-				rawLeft = mov
-				rawRight = turn
-			else:
-				rawRight = mov
-				rawLeft = turn
+		# First and third quadrant
+		if (x >= 0 and y >= 0) or (x < 0 and y < 0):
+			rawLeft = mov
+			rawRight = turn
+		else:
+			rawRight = mov
+			rawLeft = turn
 
-			if y < 0:
-				return [-rawLeft, -rawRight]
+		if y < 0:
+			return [-rawLeft, -rawRight]
 
-			return [rawRight, rawLeft]
+		return [rawRight, rawLeft]
 
 drive = DiffDrive()
 rospy.spin()
